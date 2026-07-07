500c92672ad27bc3488c88b20889de99f025fcb1
axie10 <cryptosushi21@gmail.com>
2026-07-07 20:22:55 +0200

    feat: finish basic swapping app with testing

--- src/SwapApp.sol
+++ src/SwapApp.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.35;
 
-contract SwapApp {}
+import "./interfaces/IV2Router02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract SwapApp {
+    address public V2Router02Address;
+
+    using SafeERC20 for IERC20;
+    
+
+    event SwapTokens(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut);
+
+    constructor(address V2Router02Address_) {
+        V2Router02Address = V2Router02Address_;
+    }
+
+    function swapTokens(uint256 amountIn, uint256 amountOutMin, address[] memory path, uint256 deadline) external {
+
+        IERC20(path[0]).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(path[0]).approve(V2Router02Address, amountIn);
+
+        uint256[] memory amounts = IV2Router02(V2Router02Address).swapExactTokensForTokens(amountIn, amountOutMin, path, msg.sender, deadline);
+
+        emit SwapTokens(path[0], path[path.length - 1], amountIn, amounts[amounts.length - 1]);
+    }
+}
+
+// 0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24
---
+++ test/SwapAppTest.t.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.35;
+
+import { Test } from "forge-std/Test.sol";
+import {SwapApp} from "../src/SwapApp.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// fork testin in arbitrum: forge test -vvvv --fork-url https://arb1.arbitrum.io/rpc --match-test
+
+contract SwapAppTest is Test {
+
+    SwapApp app;
+    address uniswapV2SwappRouterAddress = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+    address user = 0x1823933ddBAB0A12cd33F4d2a4A630F4549a62EF; // Address with USDT in Arbitrum Mainnet
+    address USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9; // USDT address in Arbitrum Mainnet
+    address DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1; // DAI address in Arbitrum Mainnet
+
+    function setUp() public {
+        app = new SwapApp(uniswapV2SwappRouterAddress);
+    }
+
+    function testHasBeenDeployedCorrectly() public view {
+        assert(app.V2Router02Address() == uniswapV2SwappRouterAddress);
+    }
+
+    function testSwapTokensCorrectly() public {
+        vm.startPrank(user);
+        uint256 amountIn = 5 * 1e6;
+        uint256 amountOutMin = 4 * 1e18;
+        IERC20(USDT).approve(address(app), amountIn);
+        uint256 deadline = 1738499328 + 1000000000;
+        address[] memory path = new address[](2);
+        path[0] = USDT;
+        path[1] = DAI;
+
+        uint256 usdtBalanceBefore = IERC20(USDT).balanceOf(user);
+        uint256 daiBalanceBefore = IERC20(DAI).balanceOf(user);
+        app.swapTokens(amountIn, amountOutMin, path, deadline);
+        uint256 usdtBalanceAfter = IERC20(USDT).balanceOf(user);
+        uint256 daiBalanceAfter = IERC20(DAI).balanceOf(user);
+
+        assert(usdtBalanceAfter == usdtBalanceBefore - amountIn);
+        assert(daiBalanceAfter > daiBalanceBefore);
+
+        vm.stopPrank();
+    }
+
+
+}
